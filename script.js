74daa61935818fd9ad66aa49a9069a032b0afb24
Prerna Sharma <>
2020-02-19 17:04:11 +0530

    added modal

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let box = document.getElementById("w-1"),
-    btn = document.getElementById("bubble-1");
+    btn = document.getElementById("bubble-1"),
+    addn = document.getElementById("add-new");
 
 btn.addEventListener(
     "click",
@@ -9,8 +10,43 @@ btn.addEventListener(
     false
 );
 
-function addNew() {
-    let node = `<img src="media/chat-bubble.png" alt="bubbles" id="bubble-1" />`;
+addn.addEventListener("click", addNew(), false);
 
-    document.getElementById("bubbles").appendChild(node);
+function addNew() {
+    let node = document.getElementById("bubble-1"),
+        text = `<img src="media/chat-bubble.png" alt="bubbles"/>`;
+    app = node + text;
+
+    node.innerHTML = app;
 }
+
+// Modal JQuery
+let addButton = document.getElementById("add-button");
+$(addButton).on("click", function() {
+    $(".modal").toggle();
+});
+
+// var dNow = new Date();
+// var localdate = $(
+//     dNow.getMonth() + 1 + "/" + dNow.getDate() + "/" + dNow.getFullYear()
+// );
+// localdate.val()
+
+$(".modal form").on("submit", function() {
+    var input = $(document.getElementById("cname"));
+    var input2 = $(document.getElementById("rtext"));
+
+    $("table").append(
+        "<tr><td> date here </td><td>" +
+        input.val() +
+        "</td><td>" +
+        input2.val() +
+        "</td></tr>"
+    );
+    input.val("");
+    input2.val("");
+    $(this)
+        .parent()
+        .hide();
+    return false;
+});
